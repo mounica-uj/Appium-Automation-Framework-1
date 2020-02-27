c3fd0081b03d995f06dfb1324f9fcf2fab4c98a3
Samuel Owoyemi <owoyemisamuelkayode@gmail.com>
2020-02-27 09:25:32 -0600

    Feed
    I added the script that let's user like and post reply on a post.

--- src/main/java/com/powderhook/appium/OR/HomeScreen.java
+++ src/main/java/com/powderhook/appium/OR/HomeScreen.java
@@ -6,6 +6,15 @@ public class HomeScreen {
 	public static String SignOutButton = "//*[@text='Sign Out']";
 	public static String AltSignOutYes = "//*[@text='YES']";
 	public static String lstCommunity = "//*[@text='Community']";
+	
 	public static String lstEvents = "//*[@text='Events']";
 	public static String lstTodaysEvent = "//*[@text='20']";
+	
+	public static String lstFeed = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.TabWidget/android.view.View[3]/android.view.View";
+	public static String lstFeedLike = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[1]/android.view.View[1]/android.view.View[8]/android.view.View[1]";
+	public static String lstFeedComment = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View[1]/android.view.View[1]/android.view.View[9]/android.view.View[1]";
+	public static String lstCommentBox = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.app.Dialog/android.view.View/android.view.View[2]/android.view.View[1]/android.view.View/android.widget.EditText";
+	
+	public static String lstFeedAddReply = "//*[@text='Add a reply']";
+	public static String ClickSend = "//*[@text='send']"; 
 }
---
+++ src/main/java/com/powderhook/appium/cases/feed/FeedAction.java
@@ -0,0 +1,51 @@
+package com.powderhook.appium.cases.feed;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.testng.annotations.Test;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.powderhook.appium.OR.CommunityScreen;
+import com.powderhook.appium.OR.HomeScreen;
+import com.powderhook.appium.OR.JoinNowScreen;
+import com.powderhook.appium.OR.LoginScreen;
+import com.powderhook.appium.base.ActionHelper;
+import com.powderhook.appium.common.LoginPage;
+import com.powderhook.appium.utilities.ReadExcel;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class FeedAction extends ActionHelper
+{
+	
+		@Test
+		public void clickFeed() throws InterruptedException 
+		{
+			WebElement clickFeed = findByXapth(HomeScreen.lstFeed);
+			clickFeed.click();
+			
+			WebElement clickTheFirstLike = findByXapth(HomeScreen.lstFeedLike);
+			clickTheFirstLike.click();
+			Thread.sleep(2000);
+			
+			WebElement clickFirstComment = findByXapth(HomeScreen.lstFeedComment);
+			clickFirstComment.click();
+			
+			WebElement clickFirstAddReply = findByXapth(HomeScreen.lstFeedAddReply);
+			clickFirstAddReply.click();
+			
+			type(HomeScreen.lstCommentBox,"I like this. Good job!");
+			
+			WebElement clickSend = findByXapth(HomeScreen.ClickSend);
+			clickSend.click();
+			Thread.sleep(8000);
+		} 
+
+}
